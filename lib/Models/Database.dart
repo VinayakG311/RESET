7c1ed3e3aeb1fea3c6e806d6be500ec07ee9e38d
VinayakG311 <96966973+VinayakG311@users.noreply.github.com>
2022-12-26 20:36:50 +0530

    Add files via upload

---
+++ lib/Models/Database.dart
@@ -0,0 +1,135 @@
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+
+
+class Database{
+  getUserbyID(String emailid) async{
+    return await FirebaseFirestore.instance.collection('Users').where('email', isEqualTo: emailid).get();
+
+  }
+  uploadUserInfo(userMap){
+    FirebaseFirestore.instance.collection('Users').add(userMap);
+    
+  }
+}
+
+class UserModel{
+  String? image;
+  String? uid;
+  String? firstname;
+  String? lastname;
+  String? email;
+  String? phoneNumber;
+  String? DateOfBirth;
+  String? Gender;
+
+  UserModel({this.uid,this.firstname,this.email,this.phoneNumber});
+  UserModel.fromMap(Map<String,dynamic> map){
+    uid = map['uid'];
+    firstname =  map['firstname'];
+    lastname = map['lastname'];
+    email = map['email'];
+    phoneNumber = map['phoneNumber'];
+    DateOfBirth = map['DateofBirth'];
+    Gender = map['Gender'];
+  }
+  Map<String,dynamic> toMap(){
+    return{
+      "uid":uid,
+      "firstname":firstname,
+      "lastname":lastname,
+      "email":email,
+      "phoneNumber":phoneNumber,
+      "DateofBirth":DateOfBirth,
+      "Gender":Gender
+    };
+
+}
+}
+
+class ChatRoomModel{
+  String? chatRoomId;
+  Map<String?,dynamic>? participants;
+  String? lastMessage;
+  ChatRoomModel({this.chatRoomId,this.participants,this.lastMessage});
+  ChatRoomModel.fromMap(Map<String,dynamic> map){
+    chatRoomId = map['chatRoomId'];
+    participants =  map['participants'];
+    lastMessage = map["lastmessage"];
+  }
+  Map<String,dynamic> toMap(){
+    return{
+      "chatRoomId":chatRoomId,
+      "participants":participants,
+      "lastmessage":lastMessage
+    };
+
+  }
+
+
+}
+class MessageModel{
+  String? messageid;
+  String? sender;
+  String? text;
+  bool? seen;
+  DateTime? createdon;
+  MessageModel({this.messageid, this.sender, this.text, this.seen, this.createdon});
+
+  MessageModel.fromMap(Map<String, dynamic> map) {
+    messageid = map["messageid"];
+    sender = map["sender"];
+    text = map["text"];
+    seen = map["seen"];
+    createdon = map["createdon"].toDate();
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      "messageid": messageid,
+      "sender": sender,
+      "text": text,
+      "seen": seen,
+      "createdon": createdon
+    };
+  }
+}
+
+class ProfessionalModel{
+  String? uid;
+  String? firstname;
+  String? lastname;
+  String? email;
+  String? phoneNumber;
+  String? DateOfBirth;
+  String? Gender;
+  String? certificate;
+  String? rating;
+  ProfessionalModel({this.uid,this.firstname,this.email,this.phoneNumber,this.certificate,this.rating});
+  ProfessionalModel.fromMap(Map<String,dynamic> map){
+    uid = map['uid'];
+    firstname =  map['firstname'];
+    lastname = map['lastname'];
+    email = map['email'];
+    phoneNumber = map['phoneNumber'];
+    DateOfBirth = map['DateofBirth'];
+    Gender = map['Gender'];
+    certificate= map["certificate"];
+    rating = map["rating"];
+  }
+  Map<String,dynamic> toMap(){
+    return{
+      "uid":uid,
+      "firstname":firstname,
+      "lastname":lastname,
+      "email":email,
+      "phoneNumber":phoneNumber,
+      "DateofBirth":DateOfBirth,
+      "Gender":Gender,
+      "certificate":certificate,
+      "rating":rating
+    };
+  }
+
+}
---
+++ lib/screens/book.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+
+class book extends StatelessWidget {
+  const book({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+
+      body:   Column(
+        children: [
+
+          Padding(
+            padding: const EdgeInsets.fromLTRB(30, 50, 25,0),
+            child: Row(children: [Text("Good Morning",style: TextStyle(fontSize: 30.0,fontWeight: FontWeight.bold),)],),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(30, 5, 25,0),
+            child: Row(children: [Text("Hi Charlie, I am Lia",style: TextStyle(fontSize: 30.0))],
+          )),
+          Row(
+            children: [
+
+              Padding(
+                padding: const EdgeInsets.fromLTRB(25,35, 5, 25),
+
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+
+                    children:[Image.asset('images/booksgirl.png',width: 150.0)]
+                  ),
+
+              ),
+
+
+              Padding(
+                padding: const EdgeInsets.fromLTRB(25,35, 0, 25),
+
+                child: Column(
+                    children:const [Text("How Are \nyou today?",style: TextStyle(fontSize: 30.0))]
+                ),
+
+              ),
+
+            ],
+          ),
+          Row(
+            //crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+
+              Padding(
+                padding: const EdgeInsets.fromLTRB(25.0,5.0,25.0,10.0),
+                child: Image.asset("images/bookEmojis/Great.png",width: 75,),
+              ),
+
+
+              Padding(
+                padding: const EdgeInsets.fromLTRB(25.0,5.0,25.0,10.0),
+                child: Image.asset("images/bookEmojis/Good.png",width: 75,),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(25.0,5.0,25.0,10.0),
+                child: Image.asset("images/bookEmojis/Ok.png",width: 75,),
+              ),
+            ],
+          ),
+          Row(
+
+            children:const [Padding(
+              padding: EdgeInsets.fromLTRB(35.0,0,40,0),
+              child: Text("Great",style: TextStyle(fontSize: 20),),
+            ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(35.0,0,40,0),
+                child: Text("Good",style: TextStyle(fontSize: 20),),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(45.0,0,40,0),
+                child: Text("Ok",style: TextStyle(fontSize: 20),),
+              )]
+          ),
+          Row(
+            //crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+
+              Padding(
+                padding: const EdgeInsets.fromLTRB(25.0,15.0,25.0,0.0),
+                child: Image.asset("images/bookEmojis/Bad.png",width: 75,),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.fromLTRB(25.0,15.0,25.0,0.0),
+                child: Image.asset("images/bookEmojis/Awful.png",width: 75,),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(25.0,15.0,25.0,0.0),
+                child: Image.asset("images/bookEmojis/Unsure.png",width: 75,),
+              ),
+            ],
+          ),
+          Row(
+
+              children:const [Padding(
+                padding: EdgeInsets.fromLTRB(40.0,0,40,0),
+                child: Text("Bad",style: TextStyle(fontSize: 20),),
+              ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(45.0,0,30,0),
+                  child: Text("Awful",style: TextStyle(fontSize: 20),),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(35.0,0,40,0),
+                  child: Text("Unsure",style: TextStyle(fontSize: 20),),
+                )]
+          ),
+
+        ],
+      ),
+
+      );
+
+  }
+}
